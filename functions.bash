23f3eb41a14ea89718c426f972a565c03cc94b43
jc00ke <jesse@jc00ke.com>
2010-12-01 09:24:22 -0800

    Adding rvm info to bash prompt

--- functions.bash
+++ functions.bash
@@ -26,8 +26,13 @@ function parse_git_dirty {
 function parse_git_branch {
       git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1$(parse_git_dirty))/"
 }
+function parse_rvm_info {
+    if [[ $(type rvm 2> /dev/null | head -n1) == "rvm is a function" ]] ; then
+        rvm info | grep GEM_PATH | sed -r 's/^.*\/(.*)@(.*)"$/\1:\2/'
+    fi
+}
 function parse_ps1 {
-    echo "${bldcyn}[\W]${txtwht}\$(parse_git_branch)\n$> "
+    echo "${bldcyn}[\W]${txtwht}\$(parse_git_branch)[$(parse_rvm_info)]\n$> "
 }
 
 export CLICOLOR=1
