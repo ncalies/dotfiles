58871760f6b83c4fd7daa0046c4c8d59b973d804
jc00ke <jesse@jc00ke.com>
2010-12-02 10:58:45 -0800

    Remove the rvm info stuff because it's slow

--- functions.bash
+++ functions.bash
@@ -26,18 +26,8 @@ function parse_git_dirty {
 function parse_git_branch {
       git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1$(parse_git_dirty))/"
 }
-function parse_rvm_info {
-    if [[ $(type rvm 2> /dev/null | head -n1) == "rvm is a function" ]] ; then
-        theruby=$(rvm info | grep full_version: | sed -r 's/^.*"(.*) \(.*"$/\1/')
-        thegemset=$(rvm info | grep gemset: | sed -r 's/^.*\"(.*)"$/:\1/')
-        if [[ $thegemset == ":" ]] ; then
-            thegemset=""
-        fi
-        echo "[${theruby}${thegemset}]"
-    fi
-}
 function parse_ps1 {
-    echo "${bldcyn}[\W]${txtwht}\$(parse_git_branch)\$(parse_rvm_info)\n$> "
+    echo "${bldcyn}[\W]${txtwht}\$(parse_git_branch)\n$> "
 }
 
 export CLICOLOR=1
