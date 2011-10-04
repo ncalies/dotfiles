99861b29c8af6b2a367728577bafa410cc79bcde
jc00ke <jesse@jc00ke.com>
2011-10-04 11:11:46 -0700

    reindent

--- functions.bash
+++ functions.bash
@@ -8,27 +8,31 @@ bldgrn="\[\e[1;32m\]" # Green
 bldcyn="\[\e[1;36m\]" # Cyan
 
 function parse_git_this_branch {
-      git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
 
 function parse_git_deleted {
-      [[ $(git status 2> /dev/null | grep deleted:) != "" ]] && echo "-"
+  [[ $(git status 2> /dev/null | grep deleted:) != "" ]] && echo "-"
 }
 function parse_git_added {
-      [[ $(git status 2> /dev/null | grep "Untracked files:") != "" ]] && echo "+"
+  [[ $(git status 2> /dev/null | grep "Untracked files:") != "" ]] && echo "+"
 }
 function parse_git_modified {
-      [[ $(git status 2> /dev/null | grep modified:) != "" ]] && echo "*"
+  [[ $(git status 2> /dev/null | grep modified:) != "" ]] && echo "*"
 }
 function parse_git_dirty {
-      echo "$(parse_git_added)$(parse_git_modified)$(parse_git_deleted)"
+  echo "$(parse_git_added)$(parse_git_modified)$(parse_git_deleted)"
 }
 function parse_git_branch {
-      git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1$(parse_git_dirty))/"
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1$(parse_git_dirty))/"
 }
 function parse_ps1 {
-    echo "${bldcyn}[\W]${txtwht}\$(parse_git_branch)\n$> "
+  echo "${bldcyn}[\W]${txtwht}\$(parse_git_branch)\n$> "
 }
+function kill_unicorn {
+  ps a | grep 'unicorn master' | awk 'NR < 2 { print $1}' | xargs kill -s TERM
+}
+alias ku="kill_unicorn"
 
 export CLICOLOR=1
 export LSCOLORS=gxgxcxdxbxegedabagacad
